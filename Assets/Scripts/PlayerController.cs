b482d6601bc7160b36c0efab9f490214de0bab4f
dav <usr21p1@gmail.com>
2023-02-21 10:59:28 -0800

    2/21 ~ Finished 4.3

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public GameObject powerup_indicator;
+
     private GameObject focal_point;
     private Rigidbody player_rb;
+    private bool has_powerup = false;
+    private float powerup_strength = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +24,36 @@ public class PlayerController : MonoBehaviour
     {
         float forward = Input.GetAxis("Vertical");
         player_rb.AddForce(focal_point.transform.forward * speed * forward);
+
+        powerup_indicator.transform.position = transform.position + new Vector3(0.0f, -0.6f, 0.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            has_powerup = true;
+            powerup_indicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (has_powerup && other.gameObject.CompareTag("Enemy"))
+        {
+            var enemy_rb = other.gameObject.GetComponent<Rigidbody>();
+            var distance_from_player = other.gameObject.transform.position - transform.position;
+
+            enemy_rb.AddForce(distance_from_player * powerup_strength, ForceMode.Impulse);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(5);
+        has_powerup = false;
+        powerup_indicator.gameObject.SetActive(false);
     }
 }
